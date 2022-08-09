a421b0e8eddd88bdf956ad071e9a4a989542d178
lintonwang <wangnt@mail.ustc.edu.cn>
2022-08-09 11:00:43 +0800

    daily commit 20220809

---
+++ MinStartValue.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class MinStartValue {
+    public int minStartValue(int[] nums) {
+        int n = nums.length;
+        int[] preSum = new int[n];
+        preSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            preSum[i] = preSum[i - 1] + nums[i];
+        }
+        int min = Arrays.stream(preSum).min().getAsInt();
+        return min >= 1 ? 1 : 1 - min;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

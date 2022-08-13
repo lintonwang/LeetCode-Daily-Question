9ad0e1787015f8231db8b7be2d65884d61d38358
Linton Wang <wntssb@gmail.com>
2022-08-13 14:08:06 +0800

    daily commit 20220813

---
+++ MaxChunksToSorted.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MaxChunksToSorted {
+    public int maxChunksToSorted(int[] arr) {
+        //存储每一个块中最大值
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int x : arr) {
+            if (stack.isEmpty() || x >= stack.peek()) {
+                stack.push(x);
+            } else {
+                int preMax = stack.pop();
+                while (!stack.isEmpty() && stack.peek() > x) stack.pop();
+                stack.push(preMax);
+            }
+        }
+        return stack.size();
+    }
+}

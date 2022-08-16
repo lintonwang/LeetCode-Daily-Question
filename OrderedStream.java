3dd07e1ca2725b5de0ba10d37711ecf91e2cc827
lintonwang <wangnt@mail.ustc.edu.cn>
2022-08-16 08:43:30 +0800

    daily commit 20220816

---
+++ OrderedStream.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderedStream {
+    String[] strings;
+    int ptr;
+
+    public OrderedStream(int n) {
+        strings = new String[n + 1];
+        ptr = 1;
+    }
+
+    public List<String> insert(int idKey, String value) {
+        strings[idKey] = value;
+        List<String> ans = new ArrayList<>();
+        while (ptr < strings.length && strings[ptr] != null) {
+            ans.add(strings[ptr]);
+            ptr++;
+        }
+        return ans;
+    }
+}

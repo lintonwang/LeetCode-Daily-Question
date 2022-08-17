de8d241d959695b06b4041171bb6483098df5cf1
lintonwang <wangnt@mail.ustc.edu.cn>
2022-08-17 18:10:38 +0800

    daily commit 20220817

---
+++ DeepestLeavesSum.java
@@ -0,0 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class DeepestLeavesSum {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int sum = 0, ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            ans = sum;
+            sum = 0;
+        }
+        return ans;
+    }
+}

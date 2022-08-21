65c6f5da4a580db9c5cffdc908776806b683a6b5
Linton Wang <wntssb@gmail.com>
2022-08-21 09:09:37 +0800

    daily commit 20220821

---
+++ _202208/src/IsPrefixOfWord.java
@@ -0,0 +1,15 @@
+package _202208.src;
+
+public class IsPrefixOfWord {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] strs = sentence.split(" ");
+        int idx = -1;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].startsWith(searchWord)) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    }
+}

6b0c7565fb58a64ddd4c08e12aabea2f4bdebfd6
lintonwang <wangnt@mail.ustc.edu.cn>
2022-08-26 15:45:43 +0800

    daily commit 20220826

---
+++ MaxProduct.java
@@ -0,0 +1,20 @@
+public class MaxProduct {
+    public int maxProduct(int[] nums) {
+        int max1 = 1, max2 = 1;
+        int min1 = 1, min2 = 1;
+        for (int num : nums) {
+            if (num > 1) {
+                if (num >= max1) {
+                    max2 = max1;
+                    max1 = num;
+                } else if (num > max2) max2 = num;
+            } else if (num < 1) {
+                if (num <= min1) {
+                    min2 = min1;
+                    min1 = num;
+                } else if (num < min2) min2 = num;
+            }
+        }
+        return Math.max((min1 - 1) * (min2 - 1), (max1 - 1) * (max2 - 1));
+    }
+}

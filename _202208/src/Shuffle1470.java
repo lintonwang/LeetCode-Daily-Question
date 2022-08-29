e2c60300794d0f7f10d319f02cbe7ad33a597534
Linton Wang <wntssb@gmail.com>
2022-08-29 21:32:17 +0800

    daily commit 20220829

---
+++ _202208/src/Shuffle1470.java
@@ -0,0 +1,26 @@
+package _202208.src;
+
+public class Shuffle1470 {
+    public int[] shuffle(int[] nums, int n) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                int j = i;
+                while (nums[i] > 0) {
+                    j = j < n ? j * 2 : 2 * (j - n) + 1;
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = -tmp;
+                }
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) nums[i] = -nums[i];
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Shuffle1470 so = new Shuffle1470();
+        so.shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3);
+    }
+}

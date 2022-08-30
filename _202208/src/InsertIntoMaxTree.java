6916933855dbb31df4b9e1712b96b6521face34d
Linton Wang <wntssb@gmail.com>
2022-08-30 18:35:23 +0800

    daily commit 20220830

---
+++ _202208/src/InsertIntoMaxTree.java
@@ -0,0 +1,40 @@
+package _202208.src;
+
+public class InsertIntoMaxTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode node = new TreeNode(val);
+        if (val > root.val) {
+            node.left = root;
+            root = node;
+        } else {
+            TreeNode parent = root;
+            TreeNode curr = root.right;
+            while (curr != null && curr.val > val) {
+                parent = curr;
+                curr = curr.right;
+            }
+            parent.right = node;
+            node.left = curr;
+        }
+        return root;
+    }
+}

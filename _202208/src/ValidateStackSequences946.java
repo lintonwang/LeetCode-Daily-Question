643275f33c07e3e6954323c745ca0ca4985b4528
lintonwang <wangnt@mail.ustc.edu.cn>
2022-08-31 14:17:11 +0800

    daily commit 20220831

---
+++ _202208/src/ValidateStackSequences946.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ValidateStackSequences946 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int idx = 0;
+        for (int x : pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && stack.peek() == popped[idx]) {
+                stack.pop();
+                idx++;
+            }
+        }
+        return idx == popped.length;
+    }
+}

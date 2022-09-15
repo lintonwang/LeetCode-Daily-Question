535c8ed4cc00b1d917a26f729c91907ed87dce0a
lintonwang <wangnt@mail.ustc.edu.cn>
2022-09-15 10:46:34 +0800

    daily commit 20220915

---
+++ _202209/FlipLights672.java
@@ -0,0 +1,14 @@
+package _202209;
+
+public class FlipLights672 {
+    public int flipLights(int n, int presses) {
+        if (presses == 0) return 1;
+        if (n == 1) return 2;
+        else if (n == 2) return presses == 1 ? 3 : 4;
+        else {
+            if (presses == 1) return 4;
+            else if (presses == 2) return 7;
+            else return 8;
+        }
+    }
+}

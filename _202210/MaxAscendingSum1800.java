dbc84166ee379159d54a65d01fc9b5bfc6834608
Linton Wang <wntssb@gmail.com>
2022-10-07 22:55:31 +0800

    daily commit 20221007

---
+++ _202210/MaxAscendingSum1800.java
@@ -0,0 +1,14 @@
+package _202210;
+
+public class MaxAscendingSum1800 {
+    public int maxAscendingSum(int[] nums) {
+        int sum = nums[0];
+        int ans = sum;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) sum += nums[i];
+            else sum = nums[i];
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}

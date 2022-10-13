74d7fcbecd3a3c05f4503aaadecff1def0c9ab56
lintonwang <wangnt@mail.ustc.edu.cn>
2022-10-13 15:17:42 +0800

    daily commit 20221013

---
+++ _202210/MaxChunksToSorted769.java
@@ -0,0 +1,13 @@
+package _202210;
+
+public class MaxChunksToSorted769 {
+    public int maxChunksToSorted(int[] arr) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) count++;
+        }
+        return count;
+    }
+}

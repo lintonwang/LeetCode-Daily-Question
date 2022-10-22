59daf871dd14480c99d94fd4b40db68b81f56492
Linton Wang <wntssb@gmail.com>
2022-10-22 22:59:01 +0800

    daily commit 20221022

---
+++ _202210/JobScheduling1235.java
@@ -0,0 +1,30 @@
+package _202210;
+
+import java.util.Arrays;
+
+public class JobScheduling1235 {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+        }
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int k = binarySearch(jobs, i - 1, jobs[i - 1][0]);
+            dp[i] = Math.max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+        }
+        return dp[n];
+    }
+
+    int binarySearch(int[][] jobs, int right, int target) {
+        int left = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (jobs[mid][1] > target) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}

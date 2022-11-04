6855b7e34e99498b817750b0fb5d9c43f7f6a09f
Linton Wang <wntssb@gmail.com>
2022-11-04 21:50:19 +0800

    daily commit 20221104

---
+++ _202210/ReachNumber754.java
@@ -0,0 +1,18 @@
+package _202210;
+
+public class ReachNumber754 {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int sum = 0;
+        for (int i = 1; ; i++) {
+            sum += i;
+            if (sum >= target) {
+                int d = sum - target;
+                if (d % 2 == 0) {
+                    return i;
+                }
+                return (sum + i + 1 - target) % 2 == 0 ? i + 1 : i + 2;
+            }
+        }
+    }
+}

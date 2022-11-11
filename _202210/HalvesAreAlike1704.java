b08be550f39165fcdf29d2611b3c2e986a5f3a28
Linton Wang <wntssb@gmail.com>
2022-11-11 22:42:39 +0800

    daily commit 20221111

---
+++ _202210/HalvesAreAlike1704.java
@@ -0,0 +1,16 @@
+package _202210;
+
+public class HalvesAreAlike1704 {
+    public boolean halvesAreAlike(String s) {
+        String before = s.substring(0, s.length() / 2);
+        String after = s.substring(s.length() / 2);
+        String meta = "aeiouAEIOU";
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (meta.indexOf(before.charAt(i)) >= 0) count1++;
+            if (meta.indexOf(after.charAt(i)) >= 0) count2++;
+        }
+        return count1 == count2;
+    }
+}

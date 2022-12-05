565c816b55751599711a90051b18d2b192bb1e5b
lintonwang <wangnt@mail.ustc.edu.cn>
2022-12-05 17:02:34 +0800

    daily commit 202211205

--- _202212/ClosestCost1774.java
+++ _202212/ClosestCost1774.java
@@ -15,6 +15,9 @@ public class ClosestCost1774 {
         }
         for (int top : toppingCosts) {
             for (int i = 0; i < 2; i++) {
+                //如果递增的话，在一轮循环里，可能会多次使用配料。比如循环之前dp[10]=true,
+                // 现在配料2元，你在处理10的时候会把dp[12]变成true，
+                // 然后在处理dp[12]的时候又会把dp[14]变成true。如果倒过来就不会有这样的问题。
                 for (int j = target; j > 0; j--) {
                     if (dp[j] && j + top > target) res = Math.min(res, j + top);
                     if (j - top > 0) dp[j] = dp[j] | dp[j - top];
